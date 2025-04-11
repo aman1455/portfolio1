d412eb5ec2c210421ddc81564a9cba99d7132e0c
ftech12 <aman.negi@ftechiz.com>
2025-04-11 12:57:48 +0530

    make dynamic route

--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -1,11 +1,15 @@
 // @flow strict
 
 import { experiences } from "@/utils/data/experience";
+import dynamic from 'next/dynamic';
 import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
 import AnimationLottie from "../../helper/animation-lottie";
-import GlowCard from "../../helper/glow-card";
+// import GlowCard from "../../helper/glow-card";
 import experience from '/public/lottie/code.json';
+const GlowCard = dynamic(() => import('../../helper/glow-card'), {
+  ssr: false,
+})
 
 function Experience() {
   return (
